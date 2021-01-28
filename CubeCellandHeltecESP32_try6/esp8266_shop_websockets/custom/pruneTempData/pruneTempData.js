4d01d006d16ac19572a25c855975084648c82bbc
Brent Shambaugh <brent.shambaugh@gmail.com>
2021-01-27 18:07:12 -0600

    move the p variable to an argument

--- CubeCellandHeltecESP32_try6/esp8266_shop_websockets/custom/pruneTempData/pruneTempData.js
+++ CubeCellandHeltecESP32_try6/esp8266_shop_websockets/custom/pruneTempData/pruneTempData.js
@@ -1,6 +1,5 @@
 /*
-function pruneTempData(heapTwo,message_ht,signature_ht,publicKey_ht) {
-   let p = 5;
+function pruneTempData(heapTwo,message_ht,signature_ht,publicKey_ht,p) {
    if(heapTwo.heap.length+1 > p) {
      for(let x = 1; x < heapTwo.heap.length - p; x++) {
          key = heapTwo.heap[x];
