e5f87ad62ae97d59fc98283eaba0dfc3ef5f98a1
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-05-12 09:31:39 -0500

    move esp32 code from arduino IDE to platformio, rename folder to esp32

--- edgeDevices/ESP32/src/main.cpp
+++ edgeDevices/ESP32/src/main.cpp
@@ -1,18 +1,10 @@
-/* Heltec Automation send communication test example
- *
- * Function:
- * 1. Send data from a CubeCell device over hardware 
- * 
- * 
- * this project also realess in GitHub:
- * https://github.com/HelTecAutomation/ASR650x-Arduino
- * */
-#include <concatenateArray.h>
-#include "Arduino.h"
+#include <Arduino.h>
+#include <SparkFun_ATECCX08a_Arduino_Library.h>   /// the latest version of this library might work with the ESP32. so you do not need to use Blinky's *-aug26.h
 #include <WiFi.h>
 #include <WebSocketClient.h>
-#include <SparkFun_ATECCX08a_Arduino_Library-aug26.h> //Click here to get the library: http://librarymanager/All#SparkFun_ATECCX08a
 #include <Wire.h>
+#include <concatenateArray.h>
+#include <protected.h>
 
 #define BUFFER_SIZE                                 30 // Define the payload size here
 
@@ -21,13 +13,12 @@ char txpacket[BUFFER_SIZE];
 boolean handshakeFailed=0;
 String data= "";
 
+// const char* ssid     = "your ssid"; // defined in protected.h
+// const char* password = "your password";  // defined in protected.h
 char path[] = "/";   //identifier of this device
-
-const char* ssid     = "";
-const char* password = ""; 
-char* host = "10.0.0.4";  //replace this ip address with the ip address of your Node.Js server /// hostname -I (in linux)
+char host[] = "10.0.0.4"; //replace this ip address with the ip address of your Node.Js server /// hostname -I (in linux)
 const int espport= 3000;
-  
+
 WebSocketClient webSocketClient;
 unsigned long previousMillis = 0;
 unsigned long currentMillis;
@@ -48,119 +39,9 @@ uint8_t bufferString[64];
 
 bool start = true;
 
-
 char type[5];
 char curve[5];
 char payload[200];
-
-void setup() {
-    Wire.begin();
-    Serial.begin(115200);
-
-  // check the ESP
-  if (atecc.begin() == true)
-  {
-    Serial.println("Successful wakeUp(). I2C connections are good.");
-  }
-  else
-  {
-    Serial.println("Device not found. Check wiring.");
-    while (1); // stall out forever
-  }
-
-  printInfo(); // see function below for library calls and data handling
-
-  /// print atecc object
-
- // Serial.println(atecc);
-
-  // We start by connecting to a WiFi network
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  delay(1000);
-  
-  wsconnect();
-}
-
-
-void loop() {
-
-  delay(1000);
-
-  if (client.connected()) {
-    // websocket needs to sendData public key, but it needs to stop doing this until it is asked again.
-    if (start == true) {
-      Serial.println("I am true");
-      webSocketClient.sendData("Here is my public key");
-      websocketSendPublicKey();
-      webSocketClient.sendData("end of sending my public key for the 1st time");
-      start = false;
-    }
-    // the getSignature and getPublicKey will process all of the requests from the Node.js server.
-    webSocketClient.getData(data); 
-
-    /// only run the code below if 0,1,2 is the first byte...length of string should be at least two
-    Serial.println("I am a connected client"); 
-
-// check that packet has one the available prefixes (0,1,2,3), and the data has a type
-    if(((data.charAt(0) == '0') || (data.charAt(0) == '1') || (data.charAt(0) == '2') || (data.charAt(0) == '3')) && (data.length() >= 5)) {
-
-        parse_packet(&data[0],type,curve,payload);
-
-        if (strncmp("0",type,1) == 0) {
-           Serial.println("Match Public Key to stored\n");
-           // some function that grabs the public key from the cryptochip and matches to what is sent
-           if(verifyKeyDID(payload)) {
-              Serial.println("You guessed my name");
-           } else {
-              Serial.println("You did not guess my name");
-           }
-        }
-
-        if (strncmp("1",type,1) == 0) {
-           Serial.println("I have a public Key\n");
-           websocketSendPublicKey();
-        }
-
-        if (strncmp("2",type,1) == 0) {
-          printf("I have a signature\n");
-          if  (strlen(payload) > 0) {
-             Serial.println("Sign the payload\n");
-              websocketGetSignature(payload);  // this needs to refactored to sign the payload
-          }
-        }
-
-        if (strncmp("3",type,1) == 0) {
-         Serial.println("I have a message\n");
-        }
-
-        voidArray(5,type);
-        voidArray(5,curve);
-        voidArray(200,payload);
-    } else {
-        Serial.println("Invalid packet");
-    }
-
-
-   }
-}
-
 
 //*********************************************************************************************************************
 //***************function definitions**********************************************************************************
@@ -245,6 +126,24 @@ void printInfo()
       Serial.println();
     }
   }
+}
+
+// Modified https://stackoverflow.com/questions/15050766/comparing-the-values-of-char-arrays-in-c#15050807
+// compares two character arrays to see if they have the same sequence of characters
+// I might need to modify this to return a pointer or something...
+int compare(char a[],char b[]){
+    if(strlen(a) > strlen(b)) {
+      for(int i=0;a[i]!='\0';i++){
+         if(a[i]!=b[i])
+             return 0;
+      }
+    } else {
+       for(int i=0;b[i]!='\0';i++){
+         if(a[i]!=b[i])
+             return 0;
+       }
+    }
+    return 1;
 }
 
 // verify that the public key matches the key:did you think it is
@@ -352,19 +251,108 @@ void parse_packet(char *source, char *type, char *curve, char *payload)
 
 }
 
-// Modified https://stackoverflow.com/questions/15050766/comparing-the-values-of-char-arrays-in-c#15050807
-// compares two character arrays to see if they have the same sequence of characters
-int compare(char a[],char b[]){
-    if(strlen(a) > strlen(b)) {
-      for(int i=0;a[i]!='\0';i++){
-         if(a[i]!=b[i])
-             return 0;
-      }
-    } else {
-       for(int i=0;b[i]!='\0';i++){
-         if(a[i]!=b[i])
-             return 0;
-       }
+void setup() {
+  Wire.begin();
+    Serial.begin(115200);
+
+  // check the ESP
+  if (atecc.begin() == true)
+  {
+    Serial.println("Successful wakeUp(). I2C connections are good.");
+  }
+  else
+  {
+    Serial.println("Device not found. Check wiring.");
+    while (1); // stall out forever
+  }
+
+  printInfo(); // see function below for library calls and data handling
+
+  /// print atecc object
+
+ // Serial.println(atecc);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  delay(1000);
+  
+  wsconnect();
+}
+
+void loop() {
+ delay(1000);
+
+  if (client.connected()) {
+    // websocket needs to sendData public key, but it needs to stop doing this until it is asked again.
+    if (start == true) {
+      Serial.println("I am true");
+      webSocketClient.sendData("Here is my public key");
+      websocketSendPublicKey();
+      webSocketClient.sendData("end of sending my public key for the 1st time");
+      start = false;
     }
-    return 1;
+    // the getSignature and getPublicKey will process all of the requests from the Node.js server.
+    webSocketClient.getData(data); 
+
+    /// only run the code below if 0,1,2 is the first byte...length of string should be at least two
+    Serial.println("I am a connected client"); 
+
+// check that packet has one the available prefixes (0,1,2,3), and the data has a type
+    if(((data.charAt(0) == '0') || (data.charAt(0) == '1') || (data.charAt(0) == '2') || (data.charAt(0) == '3')) && (data.length() >= 5)) {
+
+        parse_packet(&data[0],type,curve,payload);
+
+        if (strncmp("0",type,1) == 0) {
+           Serial.println("Match Public Key to stored\n");
+           // some function that grabs the public key from the cryptochip and matches to what is sent
+           if(verifyKeyDID(payload)) {
+              Serial.println("You guessed my name");
+           } else {
+              Serial.println("You did not guess my name");
+           }
+        }
+
+        if (strncmp("1",type,1) == 0) {
+           Serial.println("I have a public Key\n");
+           websocketSendPublicKey();
+        }
+
+        if (strncmp("2",type,1) == 0) {
+          printf("I have a signature\n");
+          if  (strlen(payload) > 0) {
+             Serial.println("Sign the payload\n");
+              websocketGetSignature(payload);  // this needs to refactored to sign the payload
+          }
+        }
+
+        if (strncmp("3",type,1) == 0) {
+         Serial.println("I have a message\n");
+        }
+
+        voidArray(5,type);
+        voidArray(5,curve);
+        voidArray(200,payload);
+    } else {
+        Serial.println("Invalid packet");
+    }
+
+
+   }
 }
